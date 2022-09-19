0d9f06adc8c40abf18d879096d18d7051f1411fa
Manjunath S <manjunathsingadi1@gmail.com>
2022-09-19 08:25:14 +0530

    modified ifelse component

--- src/components/IfElseCondition.vue
+++ src/components/IfElseCondition.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="ifelse">
-        <h2 v-if="show">If Else Condition</h2>
+        <h2 v-if="show">Data is being shown. Please click the button to hide the data</h2>
 
         <!-- <h2 v-else>Executing the else condition</h2> -->
         <button v-on:click="show=!show">Hide</button>
